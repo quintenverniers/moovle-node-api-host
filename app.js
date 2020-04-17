a84d2b52787bb1bca2cd3125f31cc7a9f724865f
Quinten Verniers <quintenverniers@gmail.com>
2020-04-17 14:15:51 +0200

    error handling added

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const gamesRoutes = require('./api/routes/games');
 const teamRoutes = require('./api/routes/teams');
 
+app.use(morgan('dev'));
+
 app.use('/games', gamesRoutes);
 app.use('/teams', teamRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
 
 module.exports = app;
