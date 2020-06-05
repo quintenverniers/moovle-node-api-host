ce476b6c23d8d85555a937bbde87b3ad11236283
Quinten Verniers <quintenverniers@gmail.com>
2020-06-05 14:53:22 +0200

    teamscontroller done

--- api/controllers/teams.js
+++ api/controllers/teams.js
@@ -32,7 +32,7 @@ exports.create_new_team = (req, res, next) => {
             teamImage: req.file.path
         });
         team.save().then(result => {
-            res.status(200).json({
+            res.status(201).json({
                 message: 'Handling POST request to /teams',
                 createdTeam: result,
                 user: req.userData
@@ -43,4 +43,71 @@ exports.create_new_team = (req, res, next) => {
             error: err
         });
     })
+}
+
+exports.get_team_by_id = (req, res, next) => {
+    const id = req.params.teamID;
+    Team.findById(id)
+    .exec()
+    .then((doc) => {
+        console.log(doc);
+        if(doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'no team found with the provided ID'
+            })
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.update_team = (req, res, next) => {
+    const id = req.params.teamID;
+    const fieldsToUpdate = {};
+    for(const field of req.body) {
+        fieldsToUpdate[field.propName] = field.value
+    }
+
+    Team.update({ _id: id },{ $set: fieldsToUpdate })
+    .exec()
+    .then((result) => {
+        res.status(200).json({ result });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.delete_team = (req, res, next) => {
+    const id = req.params.teamID;
+    Team.findById(id)
+    .exec()
+    .then((doc) => {
+        if(doc) {
+            return TeamStats.deleteOne({_id: doc.teamstats}).exec()
+        }
+    })
+    .then(() => {
+        return Team.deleteOne({_id: id}).exec()
+    })
+    .then((teamresult => {
+        res.status(200).json(teamresult);
+    }))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'team was not removed',
+            error: err
+        });
+    });
+    
 }
--- api/routes/teams.js
+++ api/routes/teams.js
@@ -43,78 +43,17 @@ router.post('/', checkAuth, upload.single('teamImage'), TeamsController.create_n
 /**
  * Retrieve a game
  */
-router.get('/:teamID', (req, res, next) => {
-    const id = req.params.teamID;
-    Team.findById(id)
-    .exec()
-    .then((doc) => {
-        console.log(doc);
-        if(doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({
-                message: 'no team found with the provided ID'
-            })
-        }
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.get('/:teamID', TeamsController.get_team_by_id);
 
 /**
  * Update a game
  */
-router.patch('/:teamID', (req, res, next) => {
-    const id = req.params.teamID;
-    const fieldsToUpdate = {};
-    for(const field of req.body) {
-        fieldsToUpdate[field.propName] = field.value
-    }
-
-    Team.update({ _id: id },{ $set: fieldsToUpdate })
-    .exec()
-    .then((result) => {
-        res.status(200).json({ result });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.patch('/:teamID', checkAuth, TeamsController.update_team);
 
 /**
  * Delete a game
  */
-router.delete('/:teamID', (req, res, next) => {
-    const id = req.params.teamID;
-    Team.findById(id)
-    .exec()
-    .then((doc) => {
-        if(doc) {
-            return TeamStats.deleteOne({_id: doc.teamstats}).exec()
-        }
-    })
-    .then(() => {
-        return Team.deleteOne({_id: id}).exec()
-    })
-    .then((teamresult => {
-        res.status(200).json(teamresult);
-    }))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            message: 'team was not removed',
-            error: err
-        });
-    });
-    
-});
+router.delete('/:teamID', checkAuth, TeamsController.delete_team);
 
 
 module.exports = router;
