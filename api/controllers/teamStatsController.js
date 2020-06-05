8717a277ae6ba5272d289253e8a406103f402647
Quinten Verniers <quintenverniers@gmail.com>
2020-06-05 15:04:54 +0200

    teamstat controller added

---
+++ api/controllers/teamStatsController.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const TeamStats = require('../models/teamStats');
+
+exports.get_all_teamStats = (req, res, next) => {
+    TeamStats.find()
+    .exec()
+    .then((teamstats) => {
+        res.status(200).json({
+            count: teamstats.length,
+            teamstats: teamstats
+        });
+    })
+    .catch(err => {
+        res.status(500).json({error: err});
+    });
+}
+
+exports.create_teamStats = (req, res, next) => {
+    const teamStats = new TeamStats({
+        _id: new mongoose.Types.ObjectId(),
+    });
+    teamStats.save()
+    .then(result => {
+        res.status(201).json({
+            message: 'Handling POST request to /teamsstats',
+            createdTeamstats: result
+        }); 
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
+    
+}
+
+exports.get_teamStat_by_id = (req, res, next) => {
+    const id = req.params.teamStatID;
+    TeamStats.findById(id)
+    .exec()
+    .then((doc) => {
+        console.log(doc);
+        if(doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message: 'no teamstats found with the provided ID'
+            })
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.update_teamStat = (req, res, next) => {
+    const id = req.params.teamStatID;
+    const fieldsToUpdate = {};
+    for(const field of req.body) {
+        fieldsToUpdate[field.propName] = field.value
+    }
+
+    TeamStats.update({ _id: id },{ $set: fieldsToUpdate })
+    .exec()
+    .then((result) => {
+        res.status(200).json({ result });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.delete_teamStat = (req, res, next) => {
+    const id = req.params.teamstatsID;
+    TeamStats.remove({ _id: id })
+    .exec()
+    .then((result) => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+}
--- api/routes/teamStats.js
+++ api/routes/teamStats.js
@@ -1,114 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-const mongoose = require('mongoose');
-
-const TeamStats = require('../models/teamStats');
-const Team = require('../models/team');
+const TeamStatsController = require('../controllers/teamStatsController');
 
 /**
  * Get all teamstats
  */
-router.get('/', (req, res, next) => {
-    TeamStats.find()
-    .exec()
-    .then((teamstats) => {
-        res.status(200).json({
-            count: teamstats.length,
-            teamstats: teamstats
-        });
-    })
-    .catch(err => {
-        res.status(500).json({error: err});
-    });
-});
+router.get('/', TeamStatsController.get_all_teamStats);
 
 /**
  * Create a teamstat
  */
-router.post('/', (req, res, next) => {
-    const teamStats = new TeamStats({
-        _id: new mongoose.Types.ObjectId(),
-    });
-    teamStats.save()
-    .then(result => {
-        res.status(201).json({
-            message: 'Handling POST request to /teamsstats',
-            createdTeamstats: result
-        }); 
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    })
-    
-});
+router.post('/', TeamStatsController.create_teamStats);
 
 /**
  * Retrieve a game
  */
-router.get('/:teamStatID', (req, res, next) => {
-    const id = req.params.teamStatID;
-    TeamStats.findById(id)
-    .exec()
-    .then((doc) => {
-        console.log(doc);
-        if(doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({
-                message: 'no teamstats found with the provided ID'
-            })
-        }
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.get('/:teamStatID', TeamStatsController.get_teamStat_by_id);
 
 /**
  * Update a teamstat
  */
-router.patch('/:teamStatID', (req, res, next) => {
-    const id = req.params.teamStatID;
-    const fieldsToUpdate = {};
-    for(const field of req.body) {
-        fieldsToUpdate[field.propName] = field.value
-    }
-
-    TeamStats.update({ _id: id },{ $set: fieldsToUpdate })
-    .exec()
-    .then((result) => {
-        res.status(200).json({ result });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.patch('/:teamStatID', TeamStatsController.update_teamStat);
 
 /**
  * Delete a game
  */
-router.delete('/:teamstatsID', (req, res, next) => {
-    const id = req.params.teamstatsID;
-    TeamStats.remove({ _id: id })
-    .exec()
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete('/:teamstatsID', TeamStatsController.delete_teamStat);
 
 
 module.exports = router;
