3efd5869d18fac8072a47d4193787dfa0b975db0
Quinten Verniers <quintenverniers@gmail.com>
2020-06-15 17:47:35 +0200

    port not working

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 5000;
 const server = https.createServer(app);
 
 server.listen(port, () => {
-    console.log("listening on"+`${ PORT }`);
+    console.log("listening on"+`${port}`);
 });
 //app.listen(port);
