0e1c9c9f3e3a9b610c89d083c7358066cdbea9af
Quinten Verniers <quintenverniers@gmail.com>
2020-06-15 17:26:21 +0200

    port console.log added

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, '0.0.0.0');
+server.listen(port, () => {
+    console.log("listening on `${ PORT }`");
+});
