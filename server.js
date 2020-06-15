aef546c2aa846305d01fe81bc22b990534f3ee83
Quinten Verniers <quintenverniers@gmail.com>
2020-06-15 17:30:05 +0200

    server removed, listen on app

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
+/*const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log("listening on `${ PORT }`");
-});
+});*/
+app.listen(port);
