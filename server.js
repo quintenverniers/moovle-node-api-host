a97bbdff9919fce4055e0785d2a5872f02a9c83a
Quinten Verniers <quintenverniers@gmail.com>
2020-06-15 17:33:48 +0200

    https added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-/*const server = http.createServer(app);
+const server = https.createServer(app);
 
 server.listen(port, () => {
     console.log("listening on `${ PORT }`");
-});*/
-app.listen(port);
+});
+//app.listen(port);
