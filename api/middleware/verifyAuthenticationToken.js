dd93dd98ec0c64862d15ec3602145b37b1ce2620
Quinten Verniers <quintenverniers@gmail.com>
2020-06-16 07:37:56 +0200

    request headers removed from console.log

--- api/middleware/verifyAuthenticationToken.js
+++ api/middleware/verifyAuthenticationToken.js
@@ -4,7 +4,6 @@ module.exports = (req, res, next) => {
     let authKey = "icSSmYBncvosHIeqsbnObyl2Y5SCfveupdcA4Oz2xAMIYT8cjHesNqNXkwqDkDB";
     try {
         const token = req.headers.authorization.split(" ")[1];
-        console.log(req.headers);
         const decodedToken = JWT.verify(token, authKey);
         const newAuthToken = JWT.sign(
             {
